76469a4e20a558dcc6adcf7dfb00c518f0bd2dcd
TheTzunder30 <leocedsan@campus.monlau.com>
2024-02-22 18:39:21 +0100

    a

--- DnD/src/pages/Game.jsx
+++ DnD/src/pages/Game.jsx
@@ -2,31 +2,33 @@ import React, { useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import DraggableItem from '../components/DraggableItem';
+import DropTarget from '../components/DropTarget';
 
 
 function Game() {
-  
-    const [list1, setList1] = useState(['Mas', 'Menos', 'Antouan']);
-    const [list2, setList2] = useState(['Rocio', 'Lucia', 'Zafra']);
-    const [list3, setList3] = useState(['Fotos', 'Pinturas', 'Prendas']);
-    const [droppedItems, setDroppedItems] = useState({
-      l1: location.state?.selections[0] || null,
-      l2: location.state?.selections[1] || null,
-      l3: location.state?.selections[2] || null,
-    });
-  
-    const handleDrop = (targetListId, item) => {
-      const newDroppedItems = { ...droppedItems };
-      newDroppedItems[targetListId] = item;
-      setDroppedItems(newDroppedItems);
-    };
-  
-    const handleConfirm = () => {
-      console.log('Palabras seleccionadas:', droppedItems);
-    };
+  const item1 = sessionStorage.getItem('item1');
+  const item2 = sessionStorage.getItem('item2');
+  const item3 = sessionStorage.getItem('item3');
+
+  const [list1, setList1] = useState(['Tormenta', 'Ex', 'Cita']);
+  const [list2, setList2] = useState(['Hijos', 'Amor', 'Drogas']);
+  const [list3, setList3] = useState(['Aventuras', 'Prostitución', 'Mujeres']);
+  const [droppedItems, setDroppedItems] = useState({
+    l1: location.state?.selections[0] || null,
+    l2: location.state?.selections[1] || null,
+    l3: location.state?.selections[2] || null,
+  });
+
+  const handleDrop = (targetListId, item) => {
+    const newDroppedItems = { ...droppedItems };
+    newDroppedItems[targetListId] = item;
+    setDroppedItems(newDroppedItems);
+  };
+
+  const handleConfirm = () => {
+  };
   return (
-    console.log('Palabras seleccionadas anteriores:', {selections :[droppedItems.l1 , droppedItems.l2 , droppedItems.l3]} ),
-<DndProvider backend={HTML5Backend}>
+    <DndProvider backend={HTML5Backend}>
       <div className="App">
         <div className="Lists">
           <div className="List">
@@ -53,19 +55,35 @@ function Game() {
 
         <div className="Story">
           <h2>Historia Inventada</h2>
-          <p style={{ margin: '0', lineHeight: '1.5', fontSize: '1.2em', textAlign: 'center' }}>
-            Con el tiempo, y
-           comenzaron a pasar más tiempo juntos.
-            Descubrieron que compartían una pasión por .
+          <p>
+            Con el tiempo, {item1} y {item2 + ' '}
+            comenzaron a pasar más tiempo juntos.
+            Descubrieron que compartían una pasión por {item3}.
             Juntos exploraron los bosques cercanos, se perdieron en conversaciones interminables y se inspiraron mutuamente en sus respectivas artes.
             Su amor floreció como un jardín en primavera, lleno de colores vibrantes y fragancias dulces.
+           </p>
+           <p> Sin embargo, el destino tenía desafíos preparados para la pareja.
+             Una <DropTarget onDrop={handleDrop} listId="l1" allowedListId="l1">
+              {droppedItems.l1 !== null ? <span style={{ fontWeight: 'bold' }}>{droppedItems.l1}</span> : '___'}
+              
+            </DropTarget> inesperada amenazó con separarlos, pero su amor demostró ser más fuerte.
+             Juntos, enfrentaron las adversidades con valentía, apoyándose mutuamente en cada paso del camino. Descubrieron que, a pesar de los desafíos, su amor solo se fortaleció con cada experiencia compartida.
+            </p>
+            <p>
+            Y así, {item1} y {item2} continuaron su viaje, construyendo una vida llena de <DropTarget onDrop={handleDrop} listId="l2" allowedListId="l2">
+              {droppedItems.l2 !== null ? <span style={{ fontWeight: 'bold' }}>{droppedItems.l2}</span> : '___'}
+            </DropTarget>,
+            <DropTarget onDrop={handleDrop} listId="l3" allowedListId="l3">
+              {droppedItems.l3 !== null ? <span style={{ fontWeight: 'bold' }}>{droppedItems.l3}</span> : '___'}
+            </DropTarget> y superando cualquier obstáculo que la vida les presentara. Su historia se convirtió en un recordatorio de que,
+              a veces, los encuentros fortuitos pueden convertirse en los capítulos más significativos de nuestras vidas.
           </p>
           <button onClick={handleConfirm}>
             Confirmar
           </button>
         </div>
       </div>
-    </DndProvider>  );
+    </DndProvider>);
 }
 
 export default Game;
--- DnD/src/pages/Home.jsx
+++ DnD/src/pages/Home.jsx
@@ -22,7 +22,11 @@ export default function Home() {
     
     if (droppedItems.l1 && droppedItems.l2 && droppedItems.l3) {
       navigate('/game');
-      history.push('/game', droppedItems.l1);
+      console.log('Palabras seleccionadas:', droppedItems.l1, droppedItems.l2, droppedItems.l3);
+      sessionStorage.setItem('item1', droppedItems.l1);
+      sessionStorage.setItem('item2', droppedItems.l2);
+      sessionStorage.setItem('item3', droppedItems.l3);
+
       
     } else {
       alert('Por favor, selecciona una opción para cada palabra antes de confirmar.');
@@ -37,6 +41,7 @@ export default function Home() {
             <h2>Palabra 1</h2>
             {list1.map((item, index) => (
               <DraggableItem key={index} item={item} listId="l1" onDrop={handleDrop} />
+              
             ))}
           </div>
 
@@ -58,10 +63,11 @@ export default function Home() {
         <div className="Story">
           {/* Historia */}
           <h2>Historia Inventada</h2>
-          <p style={{ margin: '0', lineHeight: '1.5', fontSize: '1.2em', textAlign: 'center' }}>
+          <p>
             Había una vez en un pequeño pueblo rodeado de verdes colinas, un joven llamado
             <DropTarget onDrop={handleDrop} listId="l1" allowedListId="l1">
               {droppedItems.l1 !== null ? <span style={{ fontWeight: 'bold' }}>{droppedItems.l1}</span> : '___'}
+              
             </DropTarget>. Un día, mientras paseaba por el mercado, sus ojos se encontraron con los de
             <DropTarget onDrop={handleDrop} listId="l2" allowedListId="l2">
               {droppedItems.l2 !== null ? <span style={{ fontWeight: 'bold' }}>{droppedItems.l2}</span> : '___'}
